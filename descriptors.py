1848d312c7c46ce43a4a9ce5a40b123702ec1748
corderjo <jose.cordero@eawag.ch>
2024-09-29 18:04:06 +0200

    changed the main script

--- descriptors.py
+++ descriptors.py
@@ -9,3 +9,4 @@ def get_maccs_fingerprints(smiles):
     transformer = FPVecTransformer(kind='maccs', dtype=float)
     maccs = transformer(smiles)
     return maccs
+
--- main.py
+++ main.py
@@ -6,34 +6,36 @@ import numpy as np
 
 from descriptors import get_maccs_fingerprints
 
-# Load the entire pipeline
-model_pipeline = joblib.load('pepper_pipeline_model.pkl')
+if __name__ == '__main__':
 
-# Streamlit app title
-st.title("PEPPER: an App to Predict Environmental Persistence ")
+    # Load the entire pipeline
+    model_pipeline = joblib.load('pepper_pipeline_model.pkl')
 
-# Upload CSV file
-uploaded_file = st.file_uploader("Upload a CSV file with chemical substance data", type="csv")
+    # Streamlit app title
+    st.title("PEPPER: an App to Predict Environmental Persistence ")
 
-if uploaded_file is not None:
-    # Load the uploaded data
-    input_data = pd.read_csv(uploaded_file)
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Upload a CSV file with chemical substance data", type="csv")
 
-    # Show the input data
-    st.write("Uploaded data:", input_data)
+    if uploaded_file is not None:
+        # Load the uploaded data
+        input_data = pd.read_csv(uploaded_file)
 
-    # # Calculate the MACCS fingerprints for the input data
-    # X = get_maccs_fingerprints(input_data.SMILES)
-    #
-    # # Use the pipeline to make predictions
-    # predicted_logB = model_pipeline.predict(X)
-    #
-    # # Convert to percentages
-    # predictions = np.round((10**predicted_logB)*100)
-    #
-    # # Show it as a dataframe
-    # predictions_df = pd.DataFrame(input_data)
-    # predictions_df['Breakthrough (%)'] = predictions
-    #
-    # # Show the predictions
-    # st.write("Predictions:", predictions_df)
+        # Show the input data
+        st.write("Uploaded data:", input_data)
+
+        # # Calculate the MACCS fingerprints for the input data
+        # X = get_maccs_fingerprints(input_data.SMILES)
+        #
+        # # Use the pipeline to make predictions
+        # predicted_logB = model_pipeline.predict(X)
+        #
+        # # Convert to percentages
+        # predictions = np.round((10**predicted_logB)*100)
+        #
+        # # Show it as a dataframe
+        # predictions_df = pd.DataFrame(input_data)
+        # predictions_df['Breakthrough (%)'] = predictions
+        #
+        # # Show the predictions
+        # st.write("Predictions:", predictions_df)
