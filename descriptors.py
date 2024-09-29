0e072c0cdc61cddf8152dc9d428c83c7d050ed6c
corderjo <jose.cordero@eawag.ch>
2024-09-29 17:44:18 +0200

    trying to figure out the problem when deploying; molfeat uses rdkit draw at some point and it seems the import the ipython console helps

--- descriptors.py
+++ descriptors.py
@@ -1,9 +1,11 @@
 # store a get_maccs_fingerprints function
+
 from molfeat.trans.fp import FPVecTransformer
 
 
 # main function
 def get_maccs_fingerprints(smiles):
+    from rdkit.Chem.Draw import IPythonConsole
     transformer = FPVecTransformer(kind='maccs', dtype=float)
     maccs = transformer(smiles)
     return maccs
