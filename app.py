a3927f0f7d9d4da669064f8f7de5bf30fc73e414
corderjo <jose.cordero@eawag.ch>
2024-09-30 19:32:06 +0200

    Fixed all requirements

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import streamlit as st
-
 import joblib
 import pandas as pd
+import streamlit as st
 import numpy as np
+
+from descriptors import get_maccs_fingerprints
 #
-# from descriptors import get_maccs_fingerprints
-#
-# from rdkit.Chem import PandasTools
+from rdkit.Chem import PandasTools
 
 
 def main():
@@ -26,27 +25,27 @@ def main():
         # Show the input data
         st.write("Uploaded data:", input_data)
 
-        # # Calculate the MACCS fingerprints for the input data
-        # X = get_maccs_fingerprints(input_data.SMILES)
-        #
-        # # Use the pipeline to make predictions
-        # predicted_logB = model_pipeline.predict(X)
-        #
-        # # Convert to percentages
-        # predictions = np.round((10**predicted_logB)*100)
-        #
-        # # Show it as a dataframe
-        # predictions_df = pd.DataFrame(input_data)
-        # predictions_df['Breakthrough (%)'] = predictions
-        #
-        # # Show the predictions
-        # # st.write("Predictions:", predictions_df)
-        #
-        # PandasTools.AddMoleculeColumnToFrame(predictions_df, smilesCol='SMILES')
-        # predictions_df.rename(columns={'ROMol': 'Structure'})
-        # predictions_df.drop(columns='SMILES', inplace=True)
-        #
-        # st.markdown(predictions_df.to_html(escape=False), unsafe_allow_html=True)
+        # Calculate the MACCS fingerprints for the input data
+        X = get_maccs_fingerprints(input_data.SMILES)
+
+        # Use the pipeline to make predictions
+        predicted_logB = model_pipeline.predict(X)
+
+        # Convert to percentages
+        predictions = np.round((10**predicted_logB)*100)
+
+        # Show it as a dataframe
+        predictions_df = pd.DataFrame(input_data)
+        predictions_df['Breakthrough (%)'] = predictions
+
+        # Show the predictions
+        # st.write("Predictions:", predictions_df)
+
+        PandasTools.AddMoleculeColumnToFrame(predictions_df, smilesCol='SMILES')
+        predictions_df.rename(columns={'ROMol': 'Structure'})
+        predictions_df.drop(columns='SMILES', inplace=True)
+
+        st.markdown(predictions_df.to_html(escape=False), unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
--- descriptors.py
+++ descriptors.py
@@ -1,8 +1,9 @@
-# from molfeat.trans.fp import FPVecTransformer
-#
-#
-# # store a get_maccs_fingerprints function
-# def get_maccs_fingerprints(smiles):
-#     transformer = FPVecTransformer(kind='maccs', dtype=float)
-#     maccs = transformer(smiles)
-#     return maccs
+
+from molfeat.trans.fp import FPVecTransformer
+# store a get_maccs_fingerprints function
+
+
+def get_maccs_fingerprints(smiles):
+    transformer = FPVecTransformer(kind='maccs', dtype=float)
+    maccs = transformer(smiles)
+    return maccs
