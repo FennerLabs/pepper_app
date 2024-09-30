1c29ed2e6e780b699260bc48c1e91d351029169e
corderjo <jose.cordero@eawag.ch>
2024-09-30 14:44:17 +0200

    removed lines realted to molfeat

--- descriptors.py
+++ descriptors.py
@@ -1,11 +1,8 @@
-
-from molfeat.trans.fp import FPVecTransformer
-
-
-# store a get_maccs_fingerprints function
-def get_maccs_fingerprints(smiles):
-    from rdkit.Chem.Draw import IPythonConsole
-    transformer = FPVecTransformer(kind='maccs', dtype=float)
-    maccs = transformer(smiles)
-    return maccs
-
+# from molfeat.trans.fp import FPVecTransformer
+#
+#
+# # store a get_maccs_fingerprints function
+# def get_maccs_fingerprints(smiles):
+#     transformer = FPVecTransformer(kind='maccs', dtype=float)
+#     maccs = transformer(smiles)
+#     return maccs
