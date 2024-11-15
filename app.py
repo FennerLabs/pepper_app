047cfd1d2b4664697d45b936eefe0f205e5eb535
corderjo <jose.cordero@eawag.ch>
2024-11-15 14:57:56 +0100

    test documentation

--- app.py
+++ app.py
@@ -1,55 +1,30 @@
-import joblib
-import pandas as pd
 import streamlit as st
-import numpy as np
 
-from descriptors import get_maccs_fingerprints
-from rdkit.Chem import PandasTools
+st.set_page_config(
+    page_title="Welcome!",
+    page_icon="👋",
+)
 
+st.write("# Welcome to Pepper web app! 👋")
 
-st.set_page_config(page_title="Batch Demo", page_icon="📈")
+st.sidebar.success("Select a demo above.")
 
+st.markdown(
+    """
+    Pepper-app is the web app version of a system developed by FennerLabs
+    (https://github.com/FennerLabs) to Predict Predict Environmental Pollutant PERsistence 
+    
+    **👈 Select a demo from the sidebar** to some of the services available 
+    
+    ### Want to beyond the web app?
+    - Check out [My own Pepper-app](https://github.com/FennerLabs/pepper_app) if you want to install the app locally. 
+      This is possible thanks to [Albert Anguera](https://github.com/anguera5)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
 
-def main():
-    # Load the entire pipeline
-    model_pipeline = joblib.load('pepper_pipeline_model.pkl')
-
-    # Streamlit app title
-    st.title("PEPPER: an app to Predict Environmental Pollutant PERsistence ")
-
-    # Upload CSV file
-    uploaded_file = st.file_uploader("Upload a CSV file with chemical substance data", type="csv")
-
-    if uploaded_file is not None:
-        # Load the uploaded data
-        input_data = pd.read_csv(uploaded_file)
-
-        # Show the input data
-        st.write("Uploaded data:", input_data)
-
-        # Calculate the MACCS fingerprints for the input data
-        X = get_maccs_fingerprints(input_data.SMILES)
-
-        # Use the pipeline to make predictions
-        predicted_logB = model_pipeline.predict(X)
-
-        # Convert to percentages
-        predictions = np.round((10**predicted_logB)*100)
-
-        # Show it as a dataframe
-        predictions_df = pd.DataFrame(input_data)
-        predictions_df['Breakthrough (%)'] = predictions
-
-        # Show the predictions
-        st.write("Predictions:", predictions_df)
-
-        PandasTools.AddMoleculeColumnToFrame(predictions_df, smilesCol='SMILES')
-        predictions_df.rename(columns={'ROMol': 'Structure'})
-        predictions_df.drop(columns='SMILES', inplace=True)
-
-        st.markdown(predictions_df.to_html(escape=False), unsafe_allow_html=True)
-
-
-if __name__ == '__main__':
-    main()
-    print('app is running')
