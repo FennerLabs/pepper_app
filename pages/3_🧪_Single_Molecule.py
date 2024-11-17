174e95079a535f2bcd2b30a229691ab66d6572a4
corderjo <jose.cordero@eawag.ch>
2024-11-17 09:40:55 +0100

    Added the model to the sinlg molecule page

--- pages/3_🧪_Single_Molecule.py
+++ pages/3_🧪_Single_Molecule.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import pandas as pd
+from rdkit import Chem
+
+from descriptors import get_maccs_fingerprints
+
+import numpy as np
+
+from rdkit.Chem import PandasTools
+
 
 st.set_page_config(
     page_title="👋 Single Molecule Predictions",
@@ -17,10 +25,8 @@ figure_names = {
     "Fluoexetine": 'CNCCC(C1=CC=CC=C1)OC2=CC=C(C=C2)C(F)(F)F',
     "Heptachlor": 'C1=CC(C2C1C3(C(=C(C2(C3(Cl)Cl)Cl)Cl)Cl)Cl)Cl',
     "Diazinon": 'CCOP(=S)(OCC)OC1=NC(=NC(=C1)C)C(C)C',
-    "4-nonylphenol": 'CCCCCCCCCC1=CC=C(C=C1)O',
-    "Figure 7": 'cccccccccccccccccc',
-    "Figure 8": 'ccccccccccccccccccccccccc',
-    "Figure 9": 'ccccccccccccccccccccccccccccc'}
+    "4-nonylphenol": 'CCCCCCCCCC1=CC=C(C=C1)O'
+}
 
 # Title and instructions
 st.write("# Enter the SMILES string for your molecule of interest")
@@ -49,14 +55,56 @@ reset = st.button("Reset", key='reset')
 
 if search_molecule or selected_from_box:
     if molecule != '':
-        st.write(f"Your input: {molecule}")
-        # Create a DataFrame to display predictions (placeholder for real logic)
-        predictions_df = pd.DataFrame()
-        predictions_df['SMILES'] = [molecule]  # Wrap in a list to avoid issues
-        predictions_df['Breakthrough'] = ['In construction']
+        # Check wether the SMILES stirng is valid
+        m = Chem.MolFromSmiles(molecule, sanitize=False)
+        if m is None:
+            st.warning('Invalid SMILES',icon='⛔')
+        else:
+            st.write(f"Your input: {molecule}")
+            st.warning('SMILES string accepted: Breakthrough will be calculated', icon='✅')
 
-        # Display the DataFrame
-        st.markdown(predictions_df.to_html(escape=False), unsafe_allow_html=True)
+            molecule = [molecule]
+
+            # Calculate the MACCS fingerprints for the input data
+
+            X = get_maccs_fingerprints(molecule)
+
+            import joblib
+            model_pipeline = joblib.load('pepper_pipeline_model.pkl')
+
+            # Use the pipeline to make predictions
+            predicted_logB = model_pipeline.predict(X)
+
+            # Convert to percentages
+
+            predictions = np.round((10 ** predicted_logB) * 100)
+
+            # Show it as a dataframe
+            predictions_df = pd.DataFrame()
+            predictions_df['SMILES'] = molecule
+            predictions_df['Breakthrough (%)'] = predictions
+
+            # Show the predictions
+            st.write("Predictions:", predictions_df)
+
+            PandasTools.AddMoleculeColumnToFrame(predictions_df, smilesCol='SMILES')
+            predictions_df.rename(columns={'ROMol': 'Structure'})
+            predictions_df.drop(columns='SMILES', inplace=True)
+
+            st.markdown(predictions_df.to_html(escape=False), unsafe_allow_html=True)
+
+            # # Create a DataFrame to display predictions (placeholder for real logic)
+            # predictions_df = pd.DataFrame()
+            # predictions_df['SMILES'] = [molecule]  # Wrap in a list to avoid issues
+            # predictions_df['Breakthrough'] = ['In construction']
+            #
+            # # Display the DataFrame
+            # st.markdown(predictions_df.to_html(escape=False), unsafe_allow_html=True)
+
+
 
 else:
     st.warning("Please enter a SMILES string")
+
+
+
