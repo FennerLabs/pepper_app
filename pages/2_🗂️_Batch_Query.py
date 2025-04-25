38d41ca36e505b54c709f18c5fe10d0456e3e6da
corderjo <jose.cordero@eawag.ch>
2025-04-25 23:16:59 +0200

    Confirmed that it works locally

--- pages/2_🗂️_Batch_Query.py
+++ pages/2_🗂️_Batch_Query.py
@@ -6,37 +6,34 @@ import numpy as np
 from prepare_input import get_maccs_fingerprints
 # from rdkit.Chem import PandasTools
 
-
-reduced_features_names = ['struct-19', 'struct-37', 'struct-38', 'struct-49', 'struct-53',
-       'struct-54', 'struct-62', 'struct-66', 'struct-72', 'struct-74',
-       'struct-77', 'struct-78', 'struct-79', 'struct-80', 'struct-82',
-       'struct-83', 'struct-84', 'struct-86', 'struct-89', 'struct-90',
-       'struct-91', 'struct-92', 'struct-93', 'struct-95', 'struct-96',
-       'struct-97', 'struct-98', 'struct-99', 'struct-100', 'struct-101',
-       'struct-104', 'struct-105', 'struct-106', 'struct-108', 'struct-109',
-       'struct-111', 'struct-112', 'struct-113', 'struct-114', 'struct-115',
-       'struct-116', 'struct-117', 'struct-118', 'struct-120', 'struct-123',
-       'struct-125', 'struct-126', 'struct-127', 'struct-128', 'struct-129',
-       'struct-131', 'struct-132', 'struct-133', 'struct-134', 'struct-135',
-       'struct-136', 'struct-137', 'struct-138', 'struct-139', 'struct-140',
-       'struct-141', 'struct-142', 'struct-144', 'struct-145', 'struct-146',
-       'struct-149', 'struct-150', 'struct-151', 'struct-152', 'struct-153',
-       'struct-154', 'struct-155', 'struct-156', 'struct-157', 'struct-158',
-       'struct-159', 'struct-160', 'struct-162', 'struct-163', 'struct-164',
-       'struct-165']
+# def tree_std_to_confidence(tree_std_array):
+#     confidence_list = []
+#     for tree_std in tree_std_array:
+#         min_tree_std = 0.29
+#         max_tree_std = 0.91
+#         if tree_std < min_tree_std:
+#             confidence = 1
+#         elif tree_std > max_tree_std:
+#             confidence = 0
+#         else:
+#             confidence = (-1/(max_tree_std-min_tree_std))*tree_std + 1.468
+#         confidence_list.append(confidence)
+#     return confidence_list
 
 
 def tree_std_to_confidence(tree_std_array):
     confidence_list = []
     for tree_std in tree_std_array:
-        min_tree_std = 0.29
-        max_tree_std = 0.91
+        # min_tree_std = 0.268560/2  # This values are based on observed deviations during training
+        # min_tree_std = 0.0
+        max_tree_std = 0.594320  # 90% threshold during training
+        min_tree_std = max_tree_std/10
         if tree_std < min_tree_std:
             confidence = 1
         elif tree_std > max_tree_std:
             confidence = 0
         else:
-            confidence = (-1/(max_tree_std-min_tree_std))*tree_std + 1.468
+            confidence = (-1 / (max_tree_std - min_tree_std)) * tree_std + (max_tree_std/(max_tree_std-min_tree_std))
         confidence_list.append(confidence)
     return confidence_list
 
@@ -44,7 +41,7 @@ def tree_std_to_confidence(tree_std_array):
 def main():
     # Load the entire pipeline
     # model_pipeline = joblib.load('pepper_pipeline_model.pkl')
-    model_regressor = joblib.load('pepper_app/pepper_wwtp_optimized_trained_regressor.pkl')
+    model_regressor = joblib.load('pepper_wwtp_optimized_trained_regressor.pkl')
 
     # Streamlit app title
     st.title("PEPPER: an app to Predict Environmental Pollutant PERsistence ")
@@ -88,12 +85,12 @@ def main():
 
         print('Preprocess data for predicting')
         # Manually apply the transformations in the pipeline to X_test, except the last estimator
-        X_test_selected = X[X.columns.isin(reduced_features_names)]
+
 
         print('Start predictions')
         # Get individual tree predictions for each instance in the test set
         individual_tree_predictions = np.array([
-            [tree.predict(X_test_selected) for tree in model_regressor.estimators_]
+            [tree.predict(X) for tree in model_regressor.estimators_]
         ]).squeeze()  # Shape: (n_estimators, n_test_samples)
 
         # Calculate mean prediction and standard deviation across tree predictions for each test sample
@@ -105,7 +102,8 @@ def main():
 
         # Use the pipeline to make predictions
         # predicted_logB = model_pipeline.predict(X)
-        predicted_logB = y_pred_means
+        raw_predictions = y_pred_means
+        predicted_logB = 1.48222333 * raw_predictions + 0.42978124623300695  # (adjustment with training data)
 
         # Convert to percentages
         predictions = np.round((10**predicted_logB)*100)
--- prepare_input.py
+++ prepare_input.py
@@ -1,4 +1,23 @@
 # store a get_maccs_fingerprints function
+import pandas as pd
+
+reduced_features_names = ['struct-19', 'struct-37', 'struct-38', 'struct-49', 'struct-53',
+       'struct-54', 'struct-62', 'struct-66', 'struct-72', 'struct-74',
+       'struct-77', 'struct-78', 'struct-79', 'struct-80', 'struct-82',
+       'struct-83', 'struct-84', 'struct-86', 'struct-89', 'struct-90',
+       'struct-91', 'struct-92', 'struct-93', 'struct-95', 'struct-96',
+       'struct-97', 'struct-98', 'struct-99', 'struct-100', 'struct-101',
+       'struct-104', 'struct-105', 'struct-106', 'struct-108', 'struct-109',
+       'struct-111', 'struct-112', 'struct-113', 'struct-114', 'struct-115',
+       'struct-116', 'struct-117', 'struct-118', 'struct-120', 'struct-123',
+       'struct-125', 'struct-126', 'struct-127', 'struct-128', 'struct-129',
+       'struct-131', 'struct-132', 'struct-133', 'struct-134', 'struct-135',
+       'struct-136', 'struct-137', 'struct-138', 'struct-139', 'struct-140',
+       'struct-141', 'struct-142', 'struct-144', 'struct-145', 'struct-146',
+       'struct-149', 'struct-150', 'struct-151', 'struct-152', 'struct-153',
+       'struct-154', 'struct-155', 'struct-156', 'struct-157', 'struct-158',
+       'struct-159', 'struct-160', 'struct-162', 'struct-163', 'struct-164',
+       'struct-165']
 
 def get_maccs_fingerprints(input_smiles):
     from pepper_lab.pepper import Pepper
@@ -10,4 +29,8 @@ def get_maccs_fingerprints(input_smiles):
     descriptors.model_data = input_smiles
     descriptors.load_descriptors(from_csv=False, MACCS=True)
     descriptors.maccs.drop(columns=descriptors.smiles_name, inplace=True)
-    return descriptors.maccs
+    reduced_matrix = descriptors.maccs.filter(items=reduced_features_names)
+    # descriptors.maccs = pd.DataFrame(descriptors.maccs)
+    # processed_X = descriptors.maccs[[descriptors.maccs.columns.intersection.isin(reduced_features_names)]]
+    # processed_X = descriptors.maccs[descriptors.maccs.columns.intersection.isin(reduced_features_names)]
+    return reduced_matrix
