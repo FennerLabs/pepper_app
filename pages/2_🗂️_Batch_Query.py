ef22f0060b6feb284780b95221f0f33a4fb82443
Jasmin Hafner <jasmin.hafner@uzh.ch>
2026-09-18 10:54:15 +0200

    FIX: trying to remove indeces again

--- pages/2_🗂️_Batch_Query.py
+++ pages/2_🗂️_Batch_Query.py
@@ -33,7 +33,7 @@ def main():
     @st.cache_data
     def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv(index=False).encode("utf-8")
+        return df.to_csv().encode("utf-8")
     example_csv = pd.read_csv('test_pepper_app.csv')
     csv = convert_df(example_csv)
 
@@ -46,7 +46,7 @@ def main():
 
     if uploaded_file is not None:
         # Load the uploaded data
-        input_data = pd.read_csv(uploaded_file)
+        input_data = pd.read_csv(uploaded_file, index=False)
 
         # Show the input data
         st.write("Uploaded data:", input_data)
